deb7fe158471f1445ce161b34a5c513853986c28
chengkk <417416576@qq.com>
2021-02-02 17:06:36 +0800

    添加插入排序注释

--- linear list/insertSort.cpp
+++ linear list/insertSort.cpp
@@ -6,12 +6,16 @@ void insertSortswap(int &value1, int &value2)
     value1 = value2;
     value2 = temp;
 }
-
+/*插入排序思想：默认a[0]已经排好序，给a[0]后面依次插入新的元素，
+ * 如果有新的元素加入，则需要将新元素插入到已有序列合适的位置中。
+ *
+ * 合适位置：因为第i轮排序后，前面i个元素都是有序的。因此只要新元素从后往前遍历，如果比末尾元素小，依次往后交换。 直到不必它小的元素停止来。这样一个数据就插入完成了。
+ * */
 void insertSort(int array[], int size)
 {
     for(int i=0; i < size-1; i++)
     {
-        int insertIndex = i+1;
+        int insertIndex = i+1;//默认插入第i+1个位置（第一次为第2个位置，第三次为第三个位置）
         for (int j = insertIndex; j >=0 ; --j) {
             if(array[j-1] > array[j])
             {
